784f94489aa1f6c72d870d08b72339aea796ce19
AITRamdaneferyal <112478535+AITRamdaneferyal@users.noreply.github.com>
2022-09-19 21:58:14 +0100

    Adjust the column width

--- user/views.py
+++ user/views.py
@@ -1,8 +1,25 @@
+
 import xlsxwriter
 
 workbook = xlsxwriter.Workbook('excelfile1.xlsx')
 worksheet = workbook.add_worksheet()
+# Add a bold format to use to highlight cells.
+cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
+cell_format.set_font_size(10)
+cell_format.set_align('center')
+widths = [{"name": 30,
+        "last_name": 30,
+        "age": 10,
+        "address": 70
+           }]
 my_header = ["name","last_name","age","address"]
+
+# Adjust the column width.
+#worksheet.set_column(1, 3, 80)  # Width of columns B:D set to 30.
+worksheet.set_column('A:A', 30)
+worksheet.set_column('B:B', 30)
+worksheet.set_column('C:C', 10)
+worksheet.set_column('D:D', 70)
 
 
 labels = [{"name": "nom",
@@ -13,7 +30,7 @@ labels = [{"name": "nom",
 
 for index1, entry in enumerate(labels):
        for index2, header in enumerate(my_header):
-           worksheet.write(index1, index2, entry[header])
+           worksheet.write(index1, index2, entry[header],cell_format)
 
 
 data = [
