80305aa9203f361cd89128740235dd5a5636fcf2
AITRamdaneferyal <112478535+AITRamdaneferyal@users.noreply.github.com>
2022-09-19 20:34:00 +0100

    create excelfile and add header and data

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+class User(models.Model):
+    nom = models.CharField(max_length=30,null=True)
+    prenom = models.CharField(max_length=30,null=True)
+    age = models.IntegerField(null=True)
+    adresse = models.CharField(max_length=80,null=True)
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import xlsxwriter
 
-# Create your views here.
+workbook = xlsxwriter.Workbook('excelfile1.xlsx')
+worksheet = workbook.add_worksheet()
+my_header = ["name","last_name","age","address"]
+
+for col_num, header in enumerate(my_header):
+    worksheet.write(0, col_num, header)
+
+data = [
+    {
+        "name": "aitramdane",
+        "last_name": "feryal",
+        "age": "24",
+        "address": "birkhadem"
+    },
+    {
+        "name": "salmi",
+        "last_name": "ahmed",
+        "age": "38",
+        "address": "babezouar"
+    }
+]
+
+for index1, entry in enumerate(data):
+       for index2, header in enumerate(my_header):
+           worksheet.write(index1+1, index2, entry[header])
+
+
+
+
+workbook.close()
